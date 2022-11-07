dff5e9a09acdaa2d23a0a23c7c1862be01c29a82
Matthias <mattwi@fysik.dtu.dk>
2022-11-07 15:14:27 +0100

    Add outfile to execute.sh

--- execute.sh
+++ execute.sh
@@ -6,5 +6,6 @@ echo "Compiling the source code ... "
 make impurities_hpc
 echo "... Done"
 
+FILE="${2%.*}" # outfilename without extension
 echo "./impurities_hpc $1 $2"
-./impurities_hpc $1 $2
+./impurities_hpc $1 $2 &> ${FILE}.out
